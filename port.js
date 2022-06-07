7561c4f568f88aec41c8528630335b3c065e0132
Zach <zachsneeringerdev@gmail.com>
2022-06-07 02:53:42 -0400

    Reponsive UI complete

--- port.js
+++ port.js
@@ -1,6 +1,22 @@
 
 // Portfolio JS
 
+const burgerIcon = document.querySelector('.burgerIcon');
+const mobileMenu = document.querySelector('.mobileMenu');
+const mobileMenuOverlay = document.querySelector('.menuOverlay');
 
+const openMobile = () => {
+    mobileMenu.classList.add('active');
+    mobileMenuOverlay.classList.add('active');
+}
+
+const closeMobile = () => {
+    mobileMenu.classList.remove('active');
+    mobileMenuOverlay.classList.remove('active');
+}
+
+
+burgerIcon.addEventListener('click', openMobile);
+mobileMenuOverlay.addEventListener('click', closeMobile);
 
 
