2427f9683397231e5742de0bca1504c0a086a30d
Zach <zachsneeringerdev@gmail.com>
2022-08-06 02:41:24 -0400

    added interactive monitor

--- port.js
+++ port.js
@@ -1,25 +1,73 @@
 
-// Portfolio JS
+// Portfolio.js
 
-const burgerIcon = document.querySelector('.burgerIcon');
-const mobileMenu = document.querySelector('.mobileMenu');
-const mobileMenuOverlay = document.querySelector('.menuOverlay');
+const mobileMenu = () => {
+    
+    // Variables
+    const burgerIcon = document.querySelector('.burgerIcon');
+    const mobileMenu = document.querySelector('.mobileMenu');
+    const mobileMenuOverlay = document.querySelector('.menuOverlay');
+ 
+    // Functions
+    const openMobile = () => {
+        mobileMenu.classList.add('active');
+        mobileMenuOverlay.classList.add('active');
+    }
 
-const openMobile = () => {
-    mobileMenu.classList.add('active');
-    mobileMenuOverlay.classList.add('active');
+    const closeMobile = () => {
+        mobileMenu.classList.remove('active');
+        mobileMenuOverlay.classList.remove('active');
+    }
+
+    const manipulateWave = (e) => {
+        theTitle.classList.add('move');
+    }
+
+    // Events
+    burgerIcon.addEventListener('click', openMobile);
+    mobileMenuOverlay.addEventListener('click', closeMobile);
 }
 
-const closeMobile = () => {
-    mobileMenu.classList.remove('active');
-    mobileMenuOverlay.classList.remove('active');
+const monitor = () => {
+
+    // using session storage to make sure start screen only runs once
+    if (sessionStorage.getItem('websiteState') === 'active') return;
+
+    const monitorSettings = () => {
+
+        // Variables
+        const startScreen = document.querySelector('.startScreen');
+        const enterButton = document.querySelector('.screen3 > button');
+        const powerButton = document.querySelector('.powerButton');
+        const closeScreenTop = document.querySelector('.shutdownScreenT');
+        const closeScreenBottom = document.querySelector('.shutdownScreenB');
+        startScreen.style.display = 'grid';
+        document.querySelector('.homePage').style.overflowY = 'hidden';
+
+        // Functions
+        const hideStartScreen = (e) => {
+            startScreen.style.display = 'none';
+            document.querySelector('.homePage').style.overflowY = 'initial';
+            sessionStorage.setItem('websiteState', 'active');
+        }
+
+        const shutdown = () => {
+            closeScreenTop.classList.toggle('show');
+            closeScreenBottom.classList.toggle('show');
+        }
+
+        // Events
+        enterButton.addEventListener('click', hideStartScreen);
+        powerButton.addEventListener('click', shutdown);
+    }
+
+    monitorSettings();
+
 }
 
-const manipulateWave = (e) => {
-    theTitle.classList.add('move');
+const portfolio = () => {
+    mobileMenu();
+    monitor();
 }
 
-burgerIcon.addEventListener('click', openMobile);
-mobileMenuOverlay.addEventListener('click', closeMobile);
-
-
+portfolio();
